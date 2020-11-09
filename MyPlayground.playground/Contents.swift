e2b5c4b1569b0bac801efb102f1a0b87ba52715b
glossyyoon <mypju@naver.com>
2020-11-09 18:35:16 +0900

    progress bar

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,14 +1,42 @@
-struct Town{
-    let name = "jiyoon"
-    var citizens = ["jiyoon", "jiwon"]
-    var resources = ["Grain": 100, "Ore":42, "Wool":75]
-    
-    func fortify(){
-        print("Defences increased!")
-    }
-}
-var myTown = Town()
+func exercise() {
 
-print(myTown.citizens)
-myTown.citizens.append("mom")
-print(myTown.citizens.count)
+    // Define the User struct here
+    struct User{
+        let name: String
+        var email: String?
+        var followers: Int
+        var isActive: Bool
+
+
+    }
+
+    init( name : String, email : String?, followers : Int, isActive : Bool) {
+        self.name = name
+        self.email = email
+        self.followers = followers
+        self.isActive = isActive
+
+        func logStatus(){
+            if isActive == true{
+                print("\(name) is working hard")
+            }
+            else{
+            print("\(name) has left earth")
+            }
+        }
+    }
+
+
+
+//    // Diagnostic code - do not change this code
+//    print("\nDiagnostic code (i.e., Challenge Hint):")
+//    var musk = User(name: "Elon", email: "elon@tesla.com", followers: 2001, isActive: true)
+//    musk.logStatus()
+//    print("Contacting \(musk.name) on \(musk.email!) ...")
+//    print("\(musk.name) has \(musk.followers) followers")
+//    // sometime later
+//    musk.isActive = false
+//    musk.logStatus()
+
+}
+ 
